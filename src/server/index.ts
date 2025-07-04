45fe742766c4d7a43a65b63398eab96b1aecfb8e
Mario González <mario@mariogl.com>
2025-07-04 12:00:01 +0200

    Add CORS

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import checkHealth from "./healthCheck/index.js";
 import handleNotFound from "./handleNotFound/index.js";
 import handleErrors from "./handleErrors/index.js";
@@ -6,6 +7,7 @@ import { applicationRouter } from "../entities/application/routes/application.ro
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", checkHealth);
